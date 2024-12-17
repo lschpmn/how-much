07e8efa9301cca6c3fbc85c1dc32df57f2646dae
lschpmn <luis@thechapman.site>
2024-12-16 23:58:33 -0600

    Calculate half-life example

--- client/components/ListContainer.tsx
+++ client/components/ListContainer.tsx
@@ -1,13 +1,16 @@
 import { Paper, Typography } from '@mui/material';
 import dayjs from 'dayjs';
 import { isEqual } from 'lodash';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Dosage } from '../../types';
 import { State } from '../types';
 
 const ListContainer = () => {
   const dosages: Dosage[] = useSelector((state: State) => state.dosages, isEqual);
+
+  const calculateHalfLife = (dosage: Dosage) =>
+    dosage.amount * Math.pow( 0.5, (Date.now() - dosage.timestamp) / (30 * 60 * 1000));
 
   return (
     <div>
@@ -22,6 +25,7 @@ const ListContainer = () => {
         >
           <Typography>Amount: {dosage.amount}</Typography>
           <Typography>Taken: {dayjs(dosage.timestamp).format('M/D/YY - h:mm A')}</Typography>
+          <Typography>Left: {calculateHalfLife(dosage)}</Typography>
         </Paper>
       ))}
     </div>
