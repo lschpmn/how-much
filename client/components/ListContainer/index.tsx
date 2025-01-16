751bb9648afd11c49cdd3951a77d0ca45706d45f
lschpmn <luis@thechapman.site>
2025-01-15 22:15:22 -0600

    Added 100 dosage limit on ListContainer

--- client/components/ListContainer/index.tsx
+++ client/components/ListContainer/index.tsx
@@ -10,7 +10,7 @@ const ListContainer = () => {
   const [showAll, setShowAll] = useState(false);
   const dosages: Dosage[] = useSelector((state: State) => state.dosages, isEqual);
 
-  const filteredDosages = dosages.filter(d => showAll || d.currentAmount > 0.5);
+  const filteredDosages = dosages.filter(d => showAll || d.currentAmount > 0.5).slice(0, 100);
 
   return (
     <div style={{ margin: '3.5rem 0 2rem 0' }}>
